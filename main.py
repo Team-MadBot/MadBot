06cca6e7609365d96790eded58f862acb85ad1e2
MadCat9958 <minecraft_yt_zh@outlook.com>
2022-05-25 17:08:22 +0300

    Ненавижу auto-import...

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-from tkinter import E
 import discord, time, datetime, os, sys
 from discord import app_commands, Forbidden
 from pypresence import Presence

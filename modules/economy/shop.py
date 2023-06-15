11dcd7153b6e3888d6079483db7f257ae2e5eff8
MadCat9958 <abolvah8@gmail.com>
2023-06-15 10:15:25 +0300

    Набросок /shop

--- modules/economy/shop.py
+++ modules/economy/shop.py
@@ -2,7 +2,7 @@ import discord
 
 from discord import app_commands
 from discord.ext import commands
-from tools import models
+from tools import models, db
 
 class EconomyShop(commands.Cog):
     def __init__(self, bot: models.MadBot):
@@ -11,7 +11,15 @@ class EconomyShop(commands.Cog):
     @app_commands.command(name="shop", description="Магазин сервера")
     @app_commands.guild_only()
     async def shop(self, interaction: discord.Interaction):
-        pass
+        guild = db.get_guild(interaction.guild.id)
+        if guild is None or guild.items == []:
+            embed = discord.Embed(
+                title="Магазин сервера (0).",
+                color=discord.Color.orange(),
+                description="*Пусто...*"
+            ).set_image("https://http.cat/204")
+            return await interaction.response.send_message(embed=embed)
+
 
 async def setup(bot: models.MadBot):
     await bot.add_cog(EconomyShop(bot))

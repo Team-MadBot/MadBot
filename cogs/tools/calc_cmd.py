0a01aa4a4ff309bf1556d668a711e5b87bf039f8
MadCat9958 <abolvah8@gmail.com>
2024-01-22 16:37:58 +0300

    rewrite cogs.tools_cog.py file into separate cogs

---
+++ cogs/tools/calc_cmd.py
@@ -0,0 +1,53 @@
+import discord
+import numexpr # type: ignore
+
+from discord.ext import commands
+from discord import app_commands
+
+from . import default_cooldown
+from classes import checks
+
+class CalcCog(commands.Cog):
+    def __init__(self, bot: commands.AutoShardedBot):
+        self.bot = bot
+
+    @app_commands.command(name="calc", description="[Полезности] Калькулятор в Discord.")
+    @app_commands.checks.dynamic_cooldown(default_cooldown)
+    @app_commands.check(checks.interaction_is_not_in_blacklist)
+    @app_commands.check(checks.interaction_is_not_shutted_down)
+    @app_commands.describe(problem="Пример для решения")
+    async def calc(self, interaction: discord.Interaction, problem: app_commands.Range[str, None, 30]):
+        if "**" in problem:
+            embed = discord.Embed(
+                title='Ошибка!',
+                color=discord.Color.red(),
+                description="Использование степени запрещено!"
+            )
+            return await interaction.response.send_message(embed=embed, ephemeral=True)
+        try:
+            answer = numexpr.evaluate(problem)  # type: ignore
+        except ZeroDivisionError:
+            embed = discord.Embed(
+                title="Ошибка!", 
+                color=discord.Color.red(),
+                description="Расскажу-ка тебе секрет. На ноль делить нельзя. У нас тут не высшая математика."
+            )
+            return await interaction.response.send_message(embed=embed, ephemeral=True)
+        except:  # FIXME: bare except
+            embed = discord.Embed(
+                title="Ошибка!",
+                color=discord.Color.red(),
+                description="Пример введён некорректно!"
+            )
+            return await interaction.response.send_message(embed=embed, ephemeral=True)
+        else:
+            embed = discord.Embed(
+                title="Калькулятор",
+                color=discord.Color.orange(),
+                description=f"Ваш пример: `{problem}`\nОтвет: `{answer}`"
+            )
+            embed.set_footer(text=str(interaction.user), icon_url=interaction.user.display_avatar.url)
+            await interaction.response.send_message(embed=embed)
+
+async def setup(bot: commands.AutoShardedBot):
+    await bot.add_cog(CalcCog(bot))
---
+++ cogs/tools/stopwatch.py
@@ -0,0 +1,48 @@
+import discord
+import time
+import datetime
+
+from discord.ext import commands
+from discord import app_commands
+
+from . import default_cooldown
+from classes import checks
+
+class StopWatchCog(commands.Cog):
+    def __init__(self, bot: commands.AutoShardedBot):
+        self.bot = bot
+
+    @app_commands.command(name="stopwatch", description="[Полезности] Секундомер.")
+    @app_commands.checks.dynamic_cooldown(default_cooldown)
+    @app_commands.check(checks.interaction_is_not_in_blacklist)
+    @app_commands.check(checks.interaction_is_not_shutted_down)
+    async def stopwatch(self, interaction: discord.Interaction):
+        embed = discord.Embed(
+            title="Секундомер", 
+            color=discord.Color.orange(), 
+            description=f"Время пошло!\nСекундомер запущен {discord.utils.format_dt(datetime.datetime.now(), 'R')}"
+        ).set_footer(
+            text=str(interaction.user),
+            icon_url=interaction.user.display_avatar.url
+        )
+
+        class Button(discord.ui.View):
+            def __init__(self, start: float):
+                super().__init__(timeout=None)
+                self.start = start
+
+            @discord.ui.button(label="Стоп", style=discord.ButtonStyle.danger)
+            async def callback(self, viewinteract: discord.Interaction, button: discord.ui.Button):  # type: ignore
+                if interaction.user.id != viewinteract.user.id:
+                    return await viewinteract.response.send_message("Не для тебя кнопочка!", ephemeral=True)
+                stop = time.time() - self.start
+                embed = discord.Embed(title="Секундомер остановлен!", color=discord.Color.red(), description=f"Насчитанное время: `{stop:.3f}s`.")
+                embed.set_footer(text=str(interaction.user), icon_url=interaction.user.display_avatar.url)
+                button.disabled = True
+                await viewinteract.response.edit_message(embed=embed, view=self)
+
+        start = time.time()
+        await interaction.response.send_message(embed=embed, view=Button(start))
+
+async def setup(bot: commands.AutoShardedBot):
+    await bot.add_cog(StopWatchCog(bot))

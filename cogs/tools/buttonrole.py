92d63e0914588eb17b6428d2a6a009a0886df218
MadCat9958 <abolvah8@gmail.com>
2024-01-24 17:56:11 +0300

    add removing disabled component in /buttonrole

--- cogs/tools/buttonrole.py
+++ cogs/tools/buttonrole.py
@@ -80,6 +80,7 @@ class ButtonRole(commands.Cog):
                 
                 self.view.roles = self.values
                 self.view.interaction = viewinteract
+                await viewinteract.message.edit(view=None)
                 self.view.stop()
         
         class SelectButtonView(discord.ui.View):
